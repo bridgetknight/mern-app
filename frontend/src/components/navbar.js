5f27c4d729c687c3bf5f247b11d7193f70c2522c
Bridget Knight <bknight@broadinstitute.org>
2024-04-02 11:51:07 -0400

    fix navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -20,21 +20,6 @@ export default function Navbar() {
   // it seems.
   return (
     <ReactNavbar bg="dark" variant="dark">
-<<<<<<< HEAD
-      <Container>
-        <Nav className="me-auto">
-          <Nav.Link href="/">Start</Nav.Link>
-          <Nav.Link href="/home">Home</Nav.Link>
-          <Nav.Link href="/privateUserProfile">Profile</Nav.Link>
-          <Nav.Link href="/mbtaAlerts">MBTA Alerts</Nav.Link>
-          <Nav.Link href="/mbtaPredictions">MBTA Routes</Nav.Link>
-          <Nav.Link href="/accountManagementPage">
-            Account Management Page
-          </Nav.Link>
-        </Nav>
-      </Container>
-    </ReactNavbar>
-=======
     <Container>
       <Nav className="me-auto">
         <Nav.Link href="/">Start</Nav.Link>
@@ -48,6 +33,5 @@ export default function Navbar() {
     </Container>
   </ReactNavbar>
 
->>>>>>> 24a54171629b5eef0d22f2c7905da834a71aa4c6
   );
 }

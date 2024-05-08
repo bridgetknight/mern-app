88b10a06a38987a85b3c4dd814c53feb0af9f8f3
sandystfort <148255580+sandystfort@users.noreply.github.com>
2024-05-08 09:50:24 -0400

    updates

--- frontend/src/components/radarmap.js
+++ frontend/src/components/radarmap.js
@@ -4,25 +4,35 @@ import * as L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 const RadarMap = ({ startingAddress, endingAddress, setMap }) => {
-    const [map, setLocalMap] = useState(null); 
+  const [map, setLocalMap] = useState(null);
 
-    // Create the map
-    useEffect(() => {
-        if (!map) {
-            const newMap = new L.map("map").setView([42.3601, -71.0589], 11);
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(newMap);
-    
-            setLocalMap(newMap);
-            setMap(newMap);
-        }
-    }, [map, setMap]);
+  // Create the map
+  useEffect(() => {
+    if (!map) {
+      const newMap = new L.map("map").setView([42.3601, -71.0589], 11);
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
+        newMap
+      );
 
-    return (
-        <MapContext.Provider value={map}>
-            <div id="map" style={{ position: "absolute", zIndex: -99, top: "154px", width: "100%", height: "100%" }} />
-        </MapContext.Provider>
-        
-    )
-}
+      setLocalMap(newMap);
+      setMap(newMap);
+    }
+  }, [map, setMap]);
+
+  return (
+    <MapContext.Provider value={map}>
+      <div
+        id="map"
+        style={{
+          position: "absolute",
+          zIndex: -99,
+          top: "154px",
+          width: "100%",
+          height: "100%",
+        }}
+      />
+    </MapContext.Provider>
+  );
+};
 
 export default RadarMap;
